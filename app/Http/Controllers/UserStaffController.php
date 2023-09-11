1f2fd68afe580eca389e924589b9b9d8398fce27
unknown <soportecontable@perucontable.com>
2023-09-11 14:38:34 -0500

    fixed

---
+++ app/Http/Controllers/UserStaffController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\user_staff;
+use Illuminate\Http\Request;
+
+class UserStaffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(user_staff $user_staff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(user_staff $user_staff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, user_staff $user_staff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(user_staff $user_staff)
+    {
+        //
+    }
+}
